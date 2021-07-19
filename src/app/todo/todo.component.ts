16de93992fe993120a1066c2ff81a927c9f0ce13
Priyanka Das <guddy.priyanka.2013@gmail.com>
2021-07-19 16:36:39 +0530

    Tag a todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { todoItemModel } from '../model/todoItem.model';
-
+import { TagModel } from '../model/tag.model';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -8,23 +8,26 @@ import { todoItemModel } from '../model/todoItem.model';
 })
 export class TodoComponent implements OnInit {
   itemList: todoItemModel[] = []; completedTask:todoItemModel[] = [];
-  @ViewChild('f') form: any;
+  @ViewChild('f') form: any; isInputValid: boolean = false; tags: TagModel[] = [{title:'work', color:'blue'},{title:'important',color:'red'}]
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onInputValueChanged(event: Event){
-    console.log(event);
+    let title = this.form.value.title.trim(); if(title.length && this.form.valid){ this.isInputValid = true;}else{this.isInputValid=false}
   }
   addItem() {
-    this.itemList.push({title: this.form.value.title, isDone: false, tag: 'add tag'})
-    this.form.reset();
-  }
+      this.itemList.push({title: this.form.value.title, isDone: false, tag: 'add tag'})
+      this.form.reset();
+ }
   updateItem(event: Event, index: number){
-    this.itemList[index].isDone = !this.itemList[index].isDone;this.completedTask = this.itemList.filter(item=> item.isDone== true);
-  }
+    this.itemList[index].isDone = !this.itemList[index].isDone; this.getCompletedItem() }
   removeItem(index: number){
-    this.itemList.splice(index, 1);
+    this.itemList.splice(index, 1);this.getCompletedItem();
   }
+  getCompletedItem(){
+    this.completedTask = this.itemList.filter(item=> item.isDone== true);
+
+  } updateTag(index: number, tag: string) { this.itemList[index].tag = tag};
 }
